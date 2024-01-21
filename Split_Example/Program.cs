6664bb7462aa44bb641fa16e0a09de79d24f371a
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-21 02:34:45 +0200

    Add project files.

---
+++ Split_Example/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Split_Example;
+
+Spliit spl = new Spliit();
+Console.WriteLine("Please Enter Your Input");
+string input = Console.ReadLine();
+Console.WriteLine(" pleace enter char to split");
+string s = Console.ReadLine();
+if (s.Length == 1)
+{
+
+    char splitc = Convert.ToChar(s);
+    try
+    {
+        spl.splitW(input, splitc);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
---
+++ Split_Example/Spliit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split_Example
+{
+    internal class Spliit
+    {
+        public void splitW(string input,char c)
+        {
+            String[] output = input.Split(c);
+            foreach (string s in output)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
